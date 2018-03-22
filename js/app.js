9a679b6978cf54ef65a9498c8b4c40d2c2f59fce
ria agnes jose <joseagnesria@gmail.com>
2018-03-21 20:49:05 -0700

    fixed the bug of duplicating image

--- js/app.js
+++ js/app.js
@@ -1,148 +1,210 @@
 'use strict';
-//alert('hii');
-var previousIndex1;
-var previousIndex2;
-var previousIndex3;
-var imageName = [];
-var imageVotes = [];
+//array to store all instances of image constructor
 var allImages = [];
-var totalClicks = 0;
 
-function Image(filepath,name){
-  this.filepath = filepath;
-  this.name = name;
+//array to keep track of previously displayed image
+var lastDisplayed =[];
+
+
+//for chart
+var imageVotes = [];
+var imageNames = [];
+//click tracker
+
+Pic.totalClicks =0;
+
+//access the image from DOM
+var rightImage = document.getElementById('product1');
+var centerImage =document.getElementById('product2');
+var leftImage = document.getElementById('product3');
+
+
+//acess div element from the DOM
+var sectionElement = document.getElementById('images');
+//acess the li element form DOM
+console.log(sectionElement);
+var listElement = document.getElementById('results');
+
+
+//constructor function
+
+function Pic (filepath,name){
+  this.name =name;
+  this.filepath=filepath;
+  this.timesDisplayed=0;
+  this.votes=0;
   allImages.push(this);
-  this.clickCount = 0;
-  this.noOfDisplay = 0;
-  imageName.push(this.name);
-  
+  imageNames.push(this.name);
 }
-new Image('images/bag.jpg','Bag');
-new Image('images/banana.jpg','Banana');
-new Image('images/bathroom.jpg','Bathroom');
-new Image('images/boots.jpg','Boots');
-new Image('images/breakfast.jpg','Breakfast');
-new Image('images/bubblegum.jpg','Bubblegum');
-new Image('images/chair.jpg','Chair');
-new Image('images/cthulhu.jpg','Cthulhu');
-new Image('images/dog-duck.jpg','Dog-duck');
-new Image('images/dragon.jpg','Dragon');
-new Image('images/pen.jpg','Pen');
-new Image('images/pet-sweep.jpg','Petsweep');
-new Image('images/scissors.jpg','Scissors');
-new Image('images/shark.jpg','Shark');
-new Image('images/sweep.png','Sweep');
-new Image('images/tauntaun.jpg','Tauntaun');
-new Image('images/unicorn.jpg','Unicorn');
-new Image('images/usb.gif','Usb');
-new Image('images/water-can.jpg','Water-can');
-new Image('images/wine-glass.jpg','Wine-glass');
+
+function createInstance() {
+
+  sectionElement.addEventListener('click',clickHandler);
+  var picsAsString = localStorage.getItem('pictures');
+  var usablePics = JSON.parse(picsAsString);
+  if ( usablePics && usablePics.length ) {
+    allImages = usablePics;
+    //show list
+    showResult();
+    //updateVotes();
+   // display chart
+   // renderChart();
+
+    return;
+    
+  }
 
 
-var image1 = document.getElementById('product1');
-var image2 = document.getElementById('product2');
-var image3 = document.getElementById('product3');
-var finalResult=document.getElementById('results');
-image1.addEventListener('click',randomImage);
-image2.addEventListener('click',randomImage);
-image3.addEventListener('click',randomImage);
+  //make all image instances
+
+  new Pic('images/bag.jpg','Bag');
+  new Pic('images/banana.jpg','Banana');
+  new Pic('images/bathroom.jpg','Bathroom');
+  new Pic('images/boots.jpg','Boots');
+  new Pic('images/breakfast.jpg','Breakfast');
+  new Pic('images/bubblegum.jpg','Bubblegum');
+  new Pic('images/chair.jpg','Chair');
+  new Pic('images/cthulhu.jpg','Cthulhu');
+  new Pic('images/dog-duck.jpg','Dog-duck');
+  new Pic('images/dragon.jpg','Dragon');
+  new Pic('images/pen.jpg','Pen');
+  new Pic('images/pet-sweep.jpg','Petsweep');
+  new Pic('images/scissors.jpg','Scissors');
+  new Pic('images/shark.jpg','Shark');
+  new Pic('images/sweep.png','Sweep');
+  new Pic('images/tauntaun.jpg','Tauntaun');
+  new Pic('images/unicorn.jpg','Unicorn');
+  new Pic('images/usb.gif','Usb');
+  new Pic('images/water-can.jpg','Water-can');
+  new Pic('images/wine-glass.jpg','Wine-glass');
+
+  //randomImages();
+}
+createInstance();
+//randomly display 3 pics.
+
+function randomImages(){
 
 
-function renderlist(){
-  for(var i = 0; i<allImages.length; i++){
-    var liElement =document.createElement('li');
-    liElement.textContent= allImages[i].name+' was shown '+ allImages[i].noOfDisplay + ' times. It got '+allImages[i].clickCount +' votes ';
-    finalResult.appendChild(liElement);
+  //generate 2 random indices
+  var randomLeft = Math.floor(Math.random() * allImages.length);
+  var randomCenter = Math.floor(Math.random() * allImages.length);
+  var randomRight = Math.floor(Math.random() * allImages.length);
+
+
+  while(randomLeft === randomCenter || lastDisplayed.includes(randomLeft) ||lastDisplayed.includes(randomRight)||lastDisplayed.includes(randomCenter)||randomCenter === randomRight||randomLeft === randomRight) {
+    console.log('Duplicate was caught!');
+    randomLeft = Math.floor(Math.random() * allImages.length);
+    console.log(randomLeft);
+    randomRight = Math.floor(Math.random() * allImages.length);
+    console.log(randomRight);
+    randomCenter = Math.floor(Math.random() * allImages.length);
+    console.log(randomCenter);
+  }
+
+  //now pics are unique,display 3 on screen
+
+  rightImage.src=allImages[randomRight].filepath;
+  rightImage.alt=allImages[randomRight].name;
+
+  centerImage.src=allImages[randomCenter].filepath;
+  centerImage.alt=allImages[randomCenter].name;
+
+  leftImage.src=allImages[randomLeft].filepath;
+  leftImage.alt=allImages[randomLeft].name;
+
+  //incrementing the no:of times it displayed
+
+  allImages[randomLeft].timesDisplayed++;
+  allImages[randomRight].timesDisplayed++;
+  allImages[randomCenter].timesDisplayed++;
+
+  //to keep track on previous image
+
+  lastDisplayed[0]=randomLeft;
+  lastDisplayed[1]=randomCenter;
+  lastDisplayed[3]=randomRight;
+}
+
+function clickHandler(event){
+  //increment clickCounter
+  Pic.totalClicks++;
+
+  //increment click on specific image
+
+  for(var i =0;i<allImages.length;i++){
+    if(event.target.alt === allImages[i].name){
+      allImages[i].votes++;
+    }
+  }
+  //check the click counter
+  //if greater than 25 then end event listner, show list,update the votes per Images,show chart
+
+  if(Pic.totalClicks>25){
+
+
+    sectionElement.removeEventListener('click',clickHandler);
+    var savePictures = JSON.stringify(allImages);
+    localStorage.setItem('pictures', savePictures);
+
+    //show list
+    showResult();
+    //update votes per Images
+    updateVotes();
+
+    //display chart
+    renderChart();
+
+  }
+  else{
+    randomImages();
+  }
+}
+function showResult(){
+  for(var i = 0; i<allImages.length;i++){
+    // target/create li element
+    var liElement = document.createElement('li');
+    //content
+    liElement.textContent=allImages[i].name + ' has ' + allImages[i].votes + ' votes was displayed ' + allImages[i].timesDisplayed;
+    //append
+    listElement.appendChild(liElement);
+
   }
 }
 
 
 function updateVotes(){
-  for(var i = 0; i<allImages.length; i++){
-    imageVotes.push(allImages[i].clickCount);
-
+  for(var i = 0;i<allImages.length;i++){
+    imageVotes[i]=allImages[i].votes;
   }
 }
+
+
+
+randomImages();
+
+
 function renderChart(){
   var context = document.getElementById('busmall-chart').getContext('2d');
-  var arrayOfColor = ['red','green','blue','yellow','gray','red','green','blue','yellow','gray','red','green','blue','yellow','gray','red','green','blue','yellow','gray','red','green','blue','yellow','gray'];
-  new Chart (context,{
-    type :'bar',
-    data:{
-      labels:imageName,
-      datasets:[{
-        label:'votes per image',
-        data:imageVotes,
-        backgroundColor:arrayOfColor,
+  var arrayOfColors=['red','green','blue','yellow','gray','red','green','blue','yellow','gray','red','green','blue','yellow','gray','red','green','blue','yellow','gray','red','green','blue','yellow','gray'];
+  new Chart(context, {
+    type: 'bar',
+    data: {
+      labels:imageNames,
+      datasets: [{
+        label: 'Votes Per Images',
+        data: imageVotes,
+        backgroundColor: arrayOfColors,
       }]
-
     },
-    options:{
-      scales:{
-        yAxes:[{
-          ticks:{
-            beginAtZero:true
-
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
           }
         }]
       }
     }
-
-
-
   });
-
 }
-
-function randomImage(event){
-  totalClicks++;
-  if(event !== null){
-    console.log(event.target.id);
-
-    //if product is 1 them take the previous index of one and increment the click count of that allImage[prevou indiex 1]
-    if(event.target.id === 'product1'){
-      allImages[previousIndex1].clickCount++;
-    }
-    else if(event.target.id === 'product2'){
-      allImages[previousIndex2].clickCount++;
-    }
-    else if(event.target.id === 'product3'){
-      allImages[previousIndex3].clickCount++;
-    }
-  }
-
-  var randomIndex =(Math.floor(Math.random()*6)+0);
-  console.log(randomIndex);
-  image1.src=allImages[randomIndex].filepath;
-  image1.alt=allImages[randomIndex].name;
-  previousIndex1 = randomIndex;
-  allImages[previousIndex1].noOfDisplay++;
-
-  var randomIndex2 =(Math.floor(Math.random()*7)+6);
-  console.log(randomIndex2);
-  image2.src=allImages[randomIndex2].filepath;
-  image2.alt=allImages[randomIndex2].name;
-  previousIndex2 = randomIndex2;
-  allImages[previousIndex2].noOfDisplay++;
-
-  var randomIndex3 =(Math.floor(Math.random()*7)+13);
-  console.log(randomIndex3);
-  image3.src=allImages[randomIndex3].filepath;
-  image3.alt=allImages[randomIndex3].name;
-  previousIndex3 = randomIndex3;
-  allImages[previousIndex3].noOfDisplay++;
-  console.log(allImages);
-
-
-  if(totalClicks>=25){
-    image1.removeEventListener('click', randomImage);
-    image2.removeEventListener('click', randomImage);
-    image3.removeEventListener('click', randomImage);
-    renderlist();
-    updateVotes();
-    renderChart();
-  }
-
-}
-
-randomImage(null);
